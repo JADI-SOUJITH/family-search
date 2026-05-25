d24091481ac3fef04150ffe79899d77793553792
JADI-SOUJITH <jadisoujith@JADIs-MacBook-Air.local>
2026-05-25 22:30:48 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const express=require("express");
+const fs=require("fs");
+
+const app=express();
+
+app.use(express.json());
+app.use(express.static("public"));
+
+app.post("/search",(req,res)=>{
+
+    const query=req.body.query;
+
+    let data=[];
+
+    if(fs.existsSync("searches.json")){
+        data=JSON.parse(fs.readFileSync("searches.json"));
+    }
+
+    data.push({
+        query,
+        time:new Date()
+    });
+
+    fs.writeFileSync("searches.json",JSON.stringify(data,null,2));
+
+    res.json({
+        redirect:`https://www.google.com/search?q=${encodeURIComponent(query)}`
+    });
+
+});
+
+app.get("/logs",(req,res)=>{
+
+    let data=[];
+
+    if(fs.existsSync("searches.json")){
+        data=JSON.parse(fs.readFileSync("searches.json"));
+    }
+
+    res.json(data.reverse());
+
+});
+
+app.listen(3000,()=>{
+    console.log("Running on port 3000");
+});
